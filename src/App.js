08b638e5976229977e3a210d0ae791f0835c4b77
Himel shipu <shipuhimel@gmail.com>
2021-06-23 15:54:49 +0600

    search filter implement

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component{
   constructor(){
     super();
     this.state ={
-      fullname:'Maksudur Rahman'
+      characters:[],
+      searchField:''
     };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users') 
+    .then(response=>response.json())
+    .then(users=>this.setState({characters:users}));
   }
   
   
   render(){
 
+    const { characters, searchField} =this.state;
+    const filteredCharacters = characters.filter(character =>
+    character.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <p></p>
-        <img src={logo} className="App-logo" alt="logo" />
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-      </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
-          Learn React
-        </a>
+    <div className='App'>
 
-      <h1> Hello {this.state.fullname}</h1>
-      <button> Change Name </button>
+    <input type='search'
+     placeholder='Search Characters' 
+        onChange = {e=> 
+          this.setState({ searchField : e.target.value } 
+          )
+    }
+    />
 
+    <CardList characters={filteredCharacters} />
 
-
-      </header>
-      
-    
     </div>
   );
 
