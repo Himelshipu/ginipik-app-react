d8ac2998ed2d7a9f93eb18bea5aef35a9326dc71
Himel shipu <shipuhimel@gmail.com>
2021-06-23 11:13:30 +0600

    project setup & class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      fullname:'Maksudur Rahman'
+    };
+  }
+  
+  
+  render(){
+
   return (
     <div className="App">
       <header className="App-header">
+      <p></p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
           Learn React
         </a>
+
+      <h1> Hello {this.state.fullname}</h1>
+      <button> Change Name </button>
+
+
+
       </header>
+      
+    
     </div>
   );
+
+  }
 }
 
 export default App;
